471fb8dafd03e99513913a2b99e942f836a7ce72
irwantsiregar <irwanto.regar30@gmail.com>
2023-04-28 21:19:58 +0700

    class

--- build/6-class.js
+++ build/6-class.js
@@ -1,1 +1,43 @@
 "use strict";
+class Root {
+}
+let User = (() => {
+    class User extends Root {
+        constructor(id, firsName, lastName) {
+            super();
+            this.retryLogin = 0;
+            this.id = id;
+            this.firstName = firsName;
+            this.lastName = lastName;
+            this.save = false;
+            this.token = '';
+            this.done = false;
+        }
+        login(username, password) {
+            this.retryLogin += 1;
+            if (username == 'admin' && password == 'admin') {
+                return true;
+            }
+            if (this.retryLogin >= User.MAX_FAILED_LOGIN) {
+                return 'max login attempted';
+            }
+            return false;
+        }
+    }
+    User.MAX_FAILED_LOGIN = 2;
+    return User;
+})();
+User.MAX_FAILED_LOGIN = 4;
+let myUser = new User(1, 'John', 'Dodo');
+console.log(myUser.login('', ''));
+console.log(myUser.login('', ''));
+console.log(myUser.login('', ''));
+console.log(myUser.login('', ''));
+class Enhancement extends User {
+    constructor(location, id, firsName, lastName) {
+        super(id, firsName, lastName);
+        this.location = location;
+        this.save;
+    }
+}
+let myNewUser = new Enhancement('medan', 2, 'Ucok', 'Cool');
--- src/6-class.ts
+++ src/6-class.ts
@@ -9,3 +9,66 @@
 
 // class --> OOP ( Object Oriented Programming)
 // class --> Object Instance
+// Object Literal vs Object Instance
+
+// implement Abstraction
+abstract class Root {
+    abstract done: boolean;
+}
+
+class User extends Root {
+    //  properties
+    id: number;
+    firstName: string;
+    lastName: string;
+    protected save: boolean;
+    private token: string;
+    static MAX_FAILED_LOGIN = 2;
+    private retryLogin = 0;
+    done: boolean;
+
+    constructor(id: number, firsName: string, lastName: string) {
+        super();
+        this.id = id;
+        this.firstName = firsName;
+        this.lastName = lastName;
+        this.save = false;
+        this.token = '';
+        this.done = false;
+    }
+
+    // method
+    login(username: string, password: string) {
+        this.retryLogin += 1;
+        if (username == 'admin' && password == 'admin') {
+            return true;
+        }
+
+        if (this.retryLogin >= User.MAX_FAILED_LOGIN) {
+            return 'max login attempted';
+        }
+
+        return false;
+    }
+
+    // register() {}
+}
+
+User.MAX_FAILED_LOGIN = 4;
+let myUser = new User(1, 'John', 'Dodo');
+console.log(myUser.login('', '')); // false
+console.log(myUser.login('', '')); // false
+console.log(myUser.login('', '')); // false
+console.log(myUser.login('', '')); // "max login attempted"
+
+// implement inheritance
+class Enhancement extends User {
+    location: string;
+    constructor(location: string, id: number, firsName: string, lastName: string) {
+        super(id, firsName, lastName);
+        this.location = location;
+        this.save;
+    }
+}
+
+let myNewUser = new Enhancement('medan', 2, 'Ucok', 'Cool');
